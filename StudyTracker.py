50034b2b2414a7bc3585baa38ebdb7607f1410e3
Falvare <77181743+Falvare@users.noreply.github.com>
2021-03-16 22:42:44 -0600

    Update StudyTracker.py

--- StudyTracker.py
+++ StudyTracker.py
@@ -1,6 +1,7 @@
 import os
 import time
 from datetime import date
+
 from plyer import notification
 
 # Change the directory to the folder where you would like to save your study tracker document
@@ -17,55 +18,88 @@ def timer(study, brk):
     time.sleep(brk)
 
 
-sbj = input('What are you studying?:')
-sbj_file = sbj.capitalize() + '.txt'
+choice = None
 
-hours = 0
-sessions = 0
+while choice != 3 or choice is None:
+    print('1.Start new study session')
+    print('2.View stats')
+    print('3.Exit')
 
-choice = 1
-while choice != sessions:
-    print('Your study session will begin in 5 seconds')
-    timer(30, 5)
-    notification.notify(title='Continue',
-                        message='You have completed your study session(s). Would you like to continue?')
-    sessions = sessions + 1
-    if sessions == sessions + 2:
-        hours = hours + 1
-    else:
-        hours = hours + .5
-    cont = input('Would you like to continue?:').capitalize()
-    if cont == 'Yes':
-        choice = choice + 1
-        continue
-    elif cont == 'No':
+    choice = input('choice(1,2,3):')
+    if choice == '1':
+        sbj = input('What are you studying?:')
+        sbj_file = sbj.capitalize() + '.txt'
+
+        hours = 0
+        sessions = 0
+
+        choice = 1
+        while choice != sessions:
+            print('Your study session will begin in 5 seconds')
+            timer(30, 5)
+            notification.notify(title='Continue',
+                                message='You have completed your study session(s). Would you like to continue?')
+            sessions = sessions + 1
+            if sessions == sessions + 2:
+                hours = hours + 1
+            else:
+                hours = hours + .5
+            cont = input('Would you like to continue?:').capitalize()
+            if cont == 'Yes':
+                choice = choice + 1
+                continue
+            elif cont == 'No':
+                break
+
+        notification.notify(title='Congratulations!', message='You have completed your study sessions for today')
+
+        today = str(date.today())
+
+        with open(sbj_file, 'a') as subject:
+            subject.write(f'DATE:{today}\nSESSIONS:{str(sessions)}\nHOURS:{str(hours)}\n\n')
+
+        session_lst = []
+        hours_lst = []
+
+        with open(sbj_file, 'r') as subject:
+            for line in subject:
+                if line.startswith('SESSIONS:'):
+                    line = line.split(':')
+                    sess = int(line[1])
+                    session_lst.append(sess)
+                elif line.startswith('HOURS:'):
+                    line = line.split(':')
+                    hrs = float(line[1])
+                    hours_lst.append(hrs)
+
+        total_hrs = str(sum(hours_lst))
+        total_sessions = str(sum(session_lst))
+
+        print('You have completed', sessions, 'sessions today')
+        print('You have completed', hours, 'hours today')
+        print('You have', total_hrs, 'total hours of study on', sbj)
+        print('you have', total_sessions, 'total', sbj, 'study sessions')
+
+    elif choice == '2':
+
+        sessions = []
+        hours = []
+
+        sbj = input('What subject would you like to view?')
+        sbj_file = sbj.capitalize() + '.txt'
+        with open(sbj_file, 'r') as subject:
+            for line in subject:
+                if line.startswith('sessions:'):
+                    line = line.split(':')
+                    sess = int(line[1])
+                    sessions.append(sess)
+                elif line.startswith('hours:'):
+                    line = line.split(':')
+                    hrs = float(line[1])
+                    hours.append(hrs)
+
+        print('You have completed', sum(sessions), 'total sessions for', sbj)
+        print('You have completed', sum(hours), 'total hours')
+
+    elif choice == '3':
         break
-
-notification.notify(title='Congratulations!', message='You have completed your study sessions for today')
-
-today = str(date.today())
-
-with open(sbj_file, 'a') as subject:
-    subject.write(f'DATE:{today}\nSESSIONS:{str(sessions)}\nHOURS:{str(hours)}\n\n')
-
-session_lst = []
-hours_lst = []
-
-with open(sbj_file, 'r') as subject:
-    for line in subject:
-        if line.startswith('SESSIONS:'):
-            line = line.split(':')
-            sess = int(line[1])
-            session_lst.append(sess)
-        elif line.startswith('HOURS:'):
-            line = line.split(':')
-            hrs = float(line[1])
-            hours_lst.append(hrs)
-
-total_hrs = str(sum(hours_lst))
-total_sessions = str(sum(session_lst))
-
-print('You have completed', sessions, 'sessions today')
-print('You have completed', hours, 'hours today')
-print('You have', total_hrs, 'total hours of study on', sbj)
-print('you have', total_sessions, 'total', sbj, 'study sessions')
